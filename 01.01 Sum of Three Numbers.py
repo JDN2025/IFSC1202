e6247f9ad74d73c1ed7c8122b8fc449f92a8c8fb
JDN2025 <jdneale@ualr.edu>
2025-01-27 16:24:14 +0000

    Content 1

---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,5 @@
+firstnumber = int(input("First Number: "))
+secondnumber = int(input("Second Number: "))
+thirdnumber = int(input("Third Number: "))
+sum = int(firstnumber) + int(secondnumber) + int(thirdnumber)
+print(sum)
